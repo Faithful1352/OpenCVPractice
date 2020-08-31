3df38e8d8a0dab2981b2fca23ae616c211291e70
Faithful1352 <aa359926262@vip.qq.com>
2020-08-31 12:50:15 +0800

    1

--- Demo2/Demo2/main.cpp
+++ Demo2/Demo2/main.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 int main() {
-
+	
+	//初始化字符数组
 	char s1[3];
 	char t1[6];
+	//初始化字符串
 	string s = "db";
 	string t = "aedbs";
+
+	//将字符串转成字符数组
 	strcpy_s(s1, s.c_str());
 	strcpy_s(t1, t.c_str());
 
 	int i = 0, j = 0;
 	while (i < (sizeof(s1) / sizeof(s1[0])) && j < (sizeof(t1) / sizeof(t1[0])))
 	{
+		//进行逐个字母比较
 		if (s1[i] == t1[j]) {
 			i++;
 			j++;
